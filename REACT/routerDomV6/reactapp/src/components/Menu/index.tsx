009f054a09744eb58c890e0f06015ddaafa6d3cb
Giovanni Henrique <giovannihpf@gmail.com>
2024-09-15 13:21:24 -0300

    Adicionando mais arquivos de aulas ao repositório do curso

---
+++ REACT/routerDomV6/reactapp/src/components/Menu/index.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+export const Menu = () => {
+    return (
+        <nav className='menu'>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+    );
+};
